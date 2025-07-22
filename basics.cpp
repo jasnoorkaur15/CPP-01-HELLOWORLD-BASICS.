87a00ee6483ed8ea03edef476ecb8a39eb051520
jasnoorkaur15 <kjasnoor15@gmail.com>
2025-07-22 23:51:21 +0530

    basics of c++.cpp

--- basics.cpp
+++ basics.cpp
@@ -1,1 +1,30 @@
+//Name : Jasnoor Kaur
+//PRN : 24070123049 ENTC A2
+#include<iostream>
+using namespace std;
 
+
+int main() {
+	float num_1, num_2, sum, diff, prod, quot;
+	cout << "Hello World" << endl;
+
+	cout << "Enter num1 :" << "\t";
+	cin >> num_1;
+
+	cout << "Enter num_2 :" << "\t";
+	cin >> num_2;
+
+	sum = num_1 + num_2;
+	cout << "Sum :" << sum << endl;
+
+	diff = num_1 - num_2;
+	cout << "Difference :" << diff << endl;
+
+	prod = num_1 * num_2;
+	cout << "Product :" << prod << endl;
+
+	quot = num_1 / num_2;
+	cout << "Division :" << quot << endl;
+
+return 0;
+}
